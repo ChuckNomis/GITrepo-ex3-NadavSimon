c576dfa9efc9c817db0e521896f1ea3164f542d5
Nadav Simon <52112772+ChuckNomis@users.noreply.github.com>
2026-04-17 23:05:27 +0300

    limon

--- NadavSimon.java
+++ NadavSimon.java
@@ -2,7 +2,7 @@ public class NadavSimon
 {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-        System.out.println("Nadav Simon ..."+i);
+        System.out.println("Nadav Limon ..."+i);
         }
     }   
 }
